8010134b3921315ca4b12a81ab13c070d2f998e8
Christiaan de Ridder <ik@christiaanderidder.nl>
2017-01-11 11:53:15 +0100

    Update version

--- woocommerce-channel-engine.php
+++ woocommerce-channel-engine.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce ChannelEngine
 Plugin URI: http://channelengine.net
 Description: ChannelEngine plugin for WooCommerce
-Version: 1.0
+Version: 1.1
 Author: ChannelEngine
 Author URI: http://channelengine.net
 */
