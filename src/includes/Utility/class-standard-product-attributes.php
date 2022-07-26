d22e7de4b844e5bb262c9f266c3c2479eea1cd1b
Zorana Radovic <zorana.radovic@logeecom.com>
2022-07-26 17:56:02 +0200

    Add list of standard product attributes
    ISSUE: CECRSET03-4

---
+++ src/includes/Utility/class-standard-product-attributes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ChannelEngine\Utility;
+
+interface Standard_Product_Attributes
+{
+    /**
+     * Unique value which will represent all standard fields
+     */
+    const PREFIX = 'ce_standard';
+
+    /**
+     * Standard WC product fields
+     */
+    const ATTRIBUTES = array(
+        'id',
+        'name',
+        'vat',
+        'stock',
+        'description',
+        'short_description',
+        'price_incl_tax',
+        'price_excl_tax',
+        'sale_price_incl_tax',
+        'sale_price_excl_tax',
+        'sku',
+        'product_url',
+        'category',
+        'image_url',
+        'weight',
+        'length',
+        'width',
+        'height'
+    );
+}
