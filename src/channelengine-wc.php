30783eeb2a8c12a2f9bc477cc94a0948591e73db
Aleksandra Panic <aleksandra.panic@logeecom.com>
2023-05-04 14:22:42 +0200

    Release version 3.7.2
    ISSUE: CECS07-50

--- src/channelengine-wc.php
+++ src/channelengine-wc.php
@@ -3,7 +3,7 @@
 Plugin Name: ChannelEngine WooCommerce Integration
 Plugin URI: https://wordpress.org/plugins/channelengine-wc/
 Description: ChannelEngine plugin for WooCommerce
-Version: 3.7.1
+Version: 3.7.2
 Text Domain: channelengine-wc
 Domain Path: /i18n/languages
 Author: ChannelEngine
