4a33cf5d656942b671a23c27fc68cca58336fb13
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2024-10-07 13:22:29 +0200

    Add security check for direct file access in all relevant PHP files.
    ISSUE: LOCO-173

--- src/copy-resources.php
+++ src/copy-resources.php
@@ -1,4 +1,8 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
 require __DIR__ . '/Lib/class-resource-copier.php';
 \ChannelEngine\Lib\Resource_Copier::copy();
