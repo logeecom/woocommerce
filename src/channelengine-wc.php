f720f2f17643685940cae257486d8ae3cd3e4f82
Milica Isailovic <milica.isailovic@logeecom.com>
2026-01-28 11:23:37 +0100

    Mark compatibility with WooCommerce 10.4.3

--- src/channelengine-wc.php
+++ src/channelengine-wc.php
@@ -10,7 +10,7 @@ Author: ChannelEngine
 Author URI: http://channelengine.net
 License: GPLv2
 WC requires at least: 3.0.0
-WC tested up to: 10.3.6
+WC tested up to: 10.4.3
 */
 
 use ChannelEngine\ChannelEngine;
