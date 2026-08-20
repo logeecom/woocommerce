7c272a71362b3f4397cd82a6c87625aa9c722d29
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2026-08-20 13:40:28 +0200

    Mark compatibility with WordPress 7.0.3 and WooCommerce 11.0.1
    ISSUE: MAIN-2705

--- src/channelengine-wc.php
+++ src/channelengine-wc.php
@@ -10,7 +10,7 @@ Author: ChannelEngine
 Author URI: http://channelengine.net
 License: GPLv2
 WC requires at least: 3.0.0
-WC tested up to: 10.9.4
+WC tested up to: 11.0.1
 */
 
 use ChannelEngine\ChannelEngine;
